88229656661cd5d8d14daaeb325e9dc880573bec
Joshua P <jwpjr567@gmail.com>
2022-05-05 18:27:35 +0000

    fix: remix config

--- remix.config.js
+++ remix.config.js
@@ -5,5 +5,6 @@ module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
 };
